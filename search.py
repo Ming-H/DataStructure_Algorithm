27d8289bf099c6ff2043a89fd6d361179bbfa85c
Ming <1518246548@qq.com>
2019-04-14 21:36:58 +0800

    Update search.py

--- search.py
+++ search.py
@@ -45,18 +45,6 @@ def insert_sort(L, target):
             return mid
     return False
       
-    
-#最大子列和问题
-def max_sum(L):
-    max = 0
-    current = 0
-    for item in L:
-        current += item
-        if current > max:
-            max = current
-        if current < 0:
-            current = 0
-    return max
 
 if __name__ == "__main__":
     L = [1, 5, 7, 8, 22, 54, 99, 123, 200, 222, 444]
