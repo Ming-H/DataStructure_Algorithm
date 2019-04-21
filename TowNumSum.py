d70c2a3eb6ec520026f1aaf6f96c56d48d5d2c20
Ming <1518246548@qq.com>
2019-04-21 14:13:04 +0800

    Update TowNumSum.py

--- TowNumSum.py
+++ TowNumSum.py
@@ -13,27 +13,23 @@ L = [1,3,5,6]
 print(towSum(L, 7))
 
 
-def combinationSum2(candidates, target):
-    """
-    和为定值的组合，每个值只能用一次
-    """
-    candidates.sort()                      
+def twosum(nums, target):
+    nums.sort()
     result = []
-    combine_sum_2(candidates, 0, [], result, target)
+    sum_method(nums, 0, [], result, target)
     return result
     
-def combine_sum_2(nums, start, path, result, target):
+def sum_method(nums, start, path, result, target):
     if not target:
         result.append(path)
-        return
+        return 
     for i in range(start, len(nums)):
-        if i > start and nums[i] == nums[i - 1]:
+        if i>start and nums[i]==nums[i-1]:
             continue
-        if nums[i] > target:
+        if nums[i]>target:
             break
-        combine_sum_2(nums, i + 1, path + [nums[i]], 
-                        result, target - nums[i])
+        sum_method(nums, start+1, path+[nums[i]], result, target-nums[i])
 
 candidates = [2,5,2,1,2]
 target = 5
-print(combinationSum2(candidates, target))
+print(twosum(candidates, target))
