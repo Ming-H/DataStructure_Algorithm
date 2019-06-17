cafa17c3948a4369f9c59d79c4a1e8ca2978ab66
Ming <1518246548@qq.com>
2019-06-17 20:09:34 +0800

    Update edit_distance.py

--- edit_distance.py
+++ edit_distance.py
@@ -5,29 +5,6 @@
 允许的编辑操作有：删除，插入，替换
 """
 
-def normal_leven(str1, str2):
-      len_str1 = len(str1) + 1
-      len_str2 = len(str2) + 1
-      matrix = [0 for n in range(len_str1 * len_str2)]
-      for i in range(len_str1):
-          matrix[i] = i
-      for j in range(0, len(matrix), len_str1):
-          if j % len_str1 == 0:
-              matrix[j] = j // len_str1
-          
-      for i in range(1, len_str1):
-          for j in range(1, len_str2):
-              if str1[i-1] == str2[j-1]:
-                  cost = 0
-              else:
-                  cost = 1
-              matrix[j*len_str1+i] = min(matrix[(j-1)*len_str1+i]+1,
-                                          matrix[j*len_str1+(i-1)]+1,
-                                          matrix[(j-1)*len_str1+(i-1)] + cost)
-          
-      return matrix[-1]
-      
-
 def edit(str1, str2):
     matrix = [[i+j for j in range(len(str2) + 1)] for i in range(len(str1) + 1)]
     for i in range(1,len(str1)+1):
